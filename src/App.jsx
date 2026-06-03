26b2c2f40b1ba63143302d24b01755250f1a32eb
mrdcabauatan <michaelarose.cabauatan@gmail.com>
2026-06-03 13:10:54 +0800

    Responsive view
    Adjust responsive view setting of all pages

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
   if (!audioRef.current) {
     audioRef.current = new Audio(bgMusic);
     audioRef.current.loop = true;
-    audioRef.current.volume = 0.8;
+    audioRef.current.volume = 0;
   }
 
   const startMusic = () => {
