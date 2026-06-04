f1ee241b958df6f0933596bbe2be80ae7416499a
mrdcabauatan <michaelarose.cabauatan@gmail.com>
2026-06-04 12:20:03 +0800

    update navigation on mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ import bgMusic from "./assets/bgmusic.mp3";
 const App = () => {
   const [currentPage, setCurrentPage] = useState("home");
   const [showIntro, setShowIntro] = useState(true);
-  const [fadeIn, setFadeIn] = useState(false);
 
   const [guestInfo, setGuestInfo] = useState({
     firstName: "",
@@ -22,8 +21,8 @@ const App = () => {
 
   const audioRef = useRef(null);
 
-  const touchStartX = useRef(0);
-  const touchEndX = useRef(0);
+  const touchStartY = useRef(0);
+  const touchEndY = useRef(0);
 
   const pages = ["home", "invitation", "details", "rsvp"];
   const currentIndex = pages.indexOf(currentPage);
@@ -47,8 +46,6 @@ const App = () => {
   const handleIntroFinish = () => {
     startMusic();
 
-    setFadeIn(false);
-
     setTimeout(() => {
       setShowIntro(false);
     }, 1200);
@@ -61,18 +58,18 @@ const App = () => {
   const handleTouchStart = (e) => {
     if (!isMobileOrTablet()) return;
 
-    touchStartX.current = e.changedTouches[0].clientX;
+    touchStartY.current = e.changedTouches[0].clientY;
   };
 
   const handleTouchEnd = (e) => {
     if (!isMobileOrTablet()) return;
 
-    touchEndX.current = e.changedTouches[0].clientX;
+    touchEndY.current = e.changedTouches[0].clientY;
 
     const swipeDistance =
-      touchStartX.current - touchEndX.current;
+      touchStartY.current - touchEndY.current;
 
-    const threshold = 50;
+    const threshold = 60;
 
     if (swipeDistance > threshold) {
       const nextIndex = Math.min(
@@ -100,7 +97,6 @@ const App = () => {
           <Navbar setCurrentPage={changePage} />
         )}
 
-        {/* Persistent Falling Petals */}
         <PetalOverlay
           active={[
             "invitation",
@@ -114,7 +110,9 @@ const App = () => {
           onTouchStart={handleTouchStart}
           onTouchEnd={handleTouchEnd}
           style={{
-            transform: `translateX(-${currentIndex * 100}vw)`,
+            transform: isMobileOrTablet()
+              ? `translateY(-${currentIndex * 100}vh)`
+              : `translateX(-${currentIndex * 100}vw)`,
           }}
         >
           <div className="page">
